9a7a2202fa1a65d3eea06185db3f4acef4749b2c
valerie chery <87587798+vchery@users.noreply.github.com>
2024-11-20 15:48:08 -0500

    added test cases for Counter

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,33 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />)
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(/Counter/i)).toBeInTheDocument()
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(/0/i)).toBeInTheDocument()
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText('+')).toBeInTheDocument()
+  expect(screen.getByText(/0/i)).toBeInTheDocument()
+  fireEvent.click(screen.getByText('+'))
+  expect(screen.getByText(/1/i)).toBeInTheDocument()
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText('-')).toBeInTheDocument()
+  expect(screen.getByText(/0/i)).toBeInTheDocument()
+  fireEvent.click(screen.getByText('-'))
+  expect(screen.getByText(/-1/i)).toBeInTheDocument()
 });
